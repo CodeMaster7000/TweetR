013208f43179e683680d206b28a03d85e6fcffaf
CodeMaster7000 <CodeMaster7K@gmail.com>
2024-04-20 22:42:40 +0100

    Add auth.py

---
+++ PlainTextTweet/auth.py
@@ -0,0 +1,5 @@
+# Add relevant details below
+consumer_key        = 'ABCDEFGHIJKLKMNOPQRSTUVWXYZ'
+consumer_secret     = 'ABCDEFGHIJKLMNOPQRSTUVXYZ1234567890'
+access_token        = 'ZYXWVUTSRQPONMLKJIHFEDCBA'
+access_token_secret = 'ZYXWVUTSRQPONMLKJIHFEDCBA0987654321'
